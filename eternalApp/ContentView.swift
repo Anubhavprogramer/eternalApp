b51689cab14e05ba7b8db23efebd7b9d7685ab3d
Anubhav <programmingwithanubhav@gmail.com>
2026-07-18 15:07:32 +0530

    Initial Commit

---
+++ eternalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eternalApp
+//
+//  Created by Anubhav Dubey on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eternalApp/eternalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eternalAppApp.swift
+//  eternalApp
+//
+//  Created by Anubhav Dubey on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct eternalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
